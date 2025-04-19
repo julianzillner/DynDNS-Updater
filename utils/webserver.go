e550d9a3a14462ee8f44c45111e5c2e67e38fafd
root <root@DESKTOP-N4LRMV0.>
2025-04-20 00:53:31 +0200

    webserver test

--- utils/webserver.go
+++ utils/webserver.go
@@ -12,6 +12,9 @@ type ipResponse struct {
 }
 
 func Initialize() {
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fs)
+
   	http.HandleFunc("/health", GetHealth)
 	http.HandleFunc("/currentIP", GetIpAdress)
     err := http.ListenAndServe(":3333", nil)
