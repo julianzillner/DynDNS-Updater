4653ed5c106e5b4a3026aeaab2e33b3674ef1df5
Julian Zillner <info@julianzillner.de>
2025-04-21 01:21:22 +0200

    update webinterface

--- static/js/script.js
+++ static/js/script.js
@@ -1,35 +1,47 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // /health abfragen
-    fetch("/health")
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById("health").textContent = data;
-        })
-        .catch(error => {
-            document.getElementById("health").textContent = "Error beim Laden des Status.";
-            console.error("/health Error:", error);
-        });
+function showSection(id) {
+  document.querySelectorAll('.content-section').forEach((el) => el.classList.remove('active'));
+  document.getElementById(id).classList.add('active');
+}
 
-    // /currentIP abfragen
-    fetch("/currentIP")
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById("currentIP").textContent = data;
-        })
-        .catch(error => {
-            document.getElementById("currentIP").textContent = "Error beim Laden der IP.";
-            console.error("/currentIP Error:", error);
-        });
+// Optional: direkt den Bereich anzeigen, wenn # in URL vorhanden
+window.addEventListener('DOMContentLoaded', () => {
+  const hash = window.location.hash.replace('#', '');
+  if (hash) {
+    showSection(hash);
+  }
+});
 
+document.addEventListener('DOMContentLoaded', function () {
+  // /health abfragen
+  fetch('/health')
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById('health').textContent = data;
+    })
+    .catch((error) => {
+      document.getElementById('health').textContent = 'Error beim Laden des Status.';
+      console.error('/health Error:', error);
+    });
 
-        // /currentIP abfragen
-    fetch("/interval/sec")
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById("interval").textContent = data;
-        })
-        .catch(error => {
-            document.getElementById("interval").textContent = "Error beim Laden der IP.";
-            console.error("/interval Error:", error);
-        });
+  // /currentIP abfragen
+  fetch('/currentIP')
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById('currentIP').textContent = data;
+    })
+    .catch((error) => {
+      document.getElementById('currentIP').textContent = 'Error beim Laden der IP.';
+      console.error('/currentIP Error:', error);
+    });
+
+  // /currentIP abfragen
+  fetch('/interval/min')
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById('interval').textContent = data;
+    })
+    .catch((error) => {
+      document.getElementById('interval').textContent = 'Error beim Laden der IP.';
+      console.error('/interval Error:', error);
+    });
 });
