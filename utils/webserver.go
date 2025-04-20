ecb677bbdcf7ea65797139a348741f04e0e00388
Julian Zillner <info@julianzillner.de>
2025-04-20 14:02:34 +0200

    update

--- utils/webserver.go
+++ utils/webserver.go
@@ -12,9 +12,8 @@ type ipResponse struct {
 }
 
 func Initialize() {
-	fs := http.FileServer(http.Dir("./static"))  
+	fs := http.FileServer(http.Dir("/app/static"))
 	http.Handle("/", fs)
-
 
   	http.HandleFunc("/health", GetHealth)
 	http.HandleFunc("/currentIP", GetIpAdress)
